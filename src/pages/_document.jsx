1d6401dadae2c601320a7c69ce64ea6abdb17f2a
Akshat Mittal <itsreallyakshat@gmail.com>
2021-04-07 02:55:19 +0530

    Update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -29,7 +29,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en" className="overflow-y-hidden">
+      <Html lang="en" className="overflow-hidden overscroll-none">
         <Head>
           <meta name="theme-color" content="#111116" />
           <link rel="stylesheet" href="https://use.typekit.net/dqh8dph.css" />
