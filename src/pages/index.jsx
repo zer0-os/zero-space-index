eac8497a14f801ada4656febf30bf0b55bca4709
Akshat Mittal <itsreallyakshat@gmail.com>
2021-08-30 15:51:53 +0530

    Update title

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="overflow-hidden">
       <Head>
-        <title>ZERO</title>
+        <title>ZERO SPACE</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HUD>
